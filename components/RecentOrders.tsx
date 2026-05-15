bf0b94f7cd52da18f9a0c2d5eea6cfefa03aba19
Kutcharaaa <kktiktok1001@gmail.com>
2026-05-15 22:43:48 +0800

    Premium Layout Redesign13

--- components/RecentOrders.tsx
+++ components/RecentOrders.tsx
@@ -32,7 +32,7 @@ export default function RecentOrders() {
 
         <a
           href="/dashboard/orders"
-          className="px-4 py-2 text-sm font-bold text-blue-600 transition hover:bg-blue-100"
+          className="px-4 py-2 text-sm font-bold text-blue-600 transition hover:text-gray-100"
         >
           View All Orders
         </a>
